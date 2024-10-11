7b6b01cdf957ea57335a106fb5dc0d58d03bd01f
José Nascimento <joseeantonioo2000@gmail.com>
2024-10-11 09:43:19 -0300

    feat: inclusion of disabling on the button if inputs are not valid

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
 
+export const ButtonContainer = styled.button<{ disable: boolean }>`
+  width: 100%;
+  height: 42px;
+  background-color: ${({ disable }) => (disable ? '#ccc' : '#81259D')};
+  color: #FFF;
+  border: 1px solid #81259D;
+  border-radius: 21px;
 
-export const ButtonContainer = styled.button`
-    width: 100%;
-    height: 42px;
-    background-color: #81259D;
-    color: #FFF;
+  &:hover {
+    opacity: ${({ disable }) => (disable ? '1' : '0.6')};
+    cursor: ${({ disable }) => (disable ? 'not-allowed' : 'pointer')};
+  }
 
-    border: 1px solid #81259D;
-    border-radius: 21px;
-
-    &:hover {
-        opacity: 0.6;
-        cursor:pointer;
-    }
-`
+  pointer-events: ${({ disable }) => (disable ? 'none' : 'auto')};
+`;
